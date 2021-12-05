cb9309c20e96e717d2c5b242b08462b5e972dcd4
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2021-12-05 15:10:36 -0300

    [2021][Day 1] Add test for file parsing

---
+++ 2021/1-sonar-sweep/utils.test.ts
@@ -0,0 +1,8 @@
+import { measurements } from './utils';
+
+describe('Day 1: Sonar sweep', () => {
+  it('parses the file correctly', () => {
+    expect(measurements.length).toEqual(2000);
+    expect(measurements.slice(0, 10)).toEqual([157, 158, 167, 157, 148, 154, 155, 156, 151, 126]);
+  });
+});
--- 2021/1-sonar-sweep/utils.ts
+++ 2021/1-sonar-sweep/utils.ts
@@ -3,6 +3,9 @@ import * as path from 'path';
 import { Measurement } from './types';
 
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf-8');
-const measurements: Measurement[] = input.split('\n').map(Number);
+const measurements: Measurement[] = input
+  .split('\n')
+  .filter((measurement) => measurement)
+  .map(Number);
 
 export { measurements };
