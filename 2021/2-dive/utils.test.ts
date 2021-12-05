37073b5d1a04a72ec9f04aeb32212481c446ef58
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2021-12-05 15:12:40 -0300

    [2021][Day 2] Add test for file parsing

---
+++ 2021/2-dive/utils.test.ts
@@ -0,0 +1,20 @@
+import { Instruction } from './types';
+import { commands } from './utils';
+
+describe('Day 1: Sonar sweep', () => {
+  it('parses the file correctly', () => {
+    expect(commands.length).toEqual(1000);
+    expect(commands.slice(0, 10)).toEqual([
+      { instruction: Instruction.Forward, size: 8 },
+      { instruction: Instruction.Forward, size: 3 },
+      { instruction: Instruction.Down, size: 8 },
+      { instruction: Instruction.Down, size: 2 },
+      { instruction: Instruction.Up, size: 1 },
+      { instruction: Instruction.Up, size: 4 },
+      { instruction: Instruction.Down, size: 9 },
+      { instruction: Instruction.Forward, size: 3 },
+      { instruction: Instruction.Forward, size: 4 },
+      { instruction: Instruction.Forward, size: 5 },
+    ]);
+  });
+});
