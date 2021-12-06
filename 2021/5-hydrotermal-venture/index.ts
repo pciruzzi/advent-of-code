c5855bbf5602c67262052726284940f71664e93c
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2021-12-05 22:20:34 -0300

    [2021][Day 5] Add missing index

---
+++ 2021/5-hydrotermal-venture/index.ts
@@ -0,0 +1,9 @@
+import { lines } from './input';
+import { solve as solvePart1 } from './part-1';
+import { solve as solvePart2 } from './part-2';
+
+console.log('Solving part 1...');
+solvePart1(lines);
+
+console.log('Solving part 2...');
+solvePart2(lines);
